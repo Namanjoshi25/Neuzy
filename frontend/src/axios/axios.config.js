bae9dcc2db438e2f8b515a705572dd1dbcf65efe
Naman joshi <84171358+Namanjoshi25@users.noreply.github.com>
2024-09-10 15:14:36 +0530

    Update axios.config.js

--- frontend/src/axios/axios.config.js
+++ frontend/src/axios/axios.config.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const defaultToken = localStorage.getItem('accessToken'); // Retrieve the token from localStorage
 
 const axiosInstance = axios.create({
-  baseURL: 'https://neuzy-backend.vercel.app/api/v1',
+  baseURL: 'https://neuzy.onrender.com/api/v1',
   timeout: 10000, // 10 seconds timeout
   headers: {
     'Content-Type': 'application/json',
